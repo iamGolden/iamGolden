0accacc835f6d3bb57956e757989bdfffb2322ea
Golden Chukwukere <chukwukeregolden@gmail.com>
2023-02-25 15:56:11 +0100

    Created 20230225155607

---
+++ run.bat
@@ -0,0 +1,1 @@
+cd "C:/Users/GOLDEN/sources/repos/iamgolden" & "C:/Users/Golden/AppData/Local/Programs/Python/Python38/python.exe" "c:/Users/Golden/Desktop/Code/Python Code/Daily-Git-Commit/main.py"
